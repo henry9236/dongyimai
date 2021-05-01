1a0aab29d0499a56929da44f8ecdea81912a30d7
Henry <henry_lin_@hotmail.com>
2021-05-01 19:57:16 +0800

    完成了specification的页的增删改查

--- dongyimai_sellergoods_service/src/main/java/com/sellergoods/service/impl/SpecificationServiceImpl.java
+++ dongyimai_sellergoods_service/src/main/java/com/sellergoods/service/impl/SpecificationServiceImpl.java
@@ -1,5 +1,10 @@
 package com.sellergoods.service.impl;
 import java.util.List;
+
+import com.dongyimai.bean.TbSpecificationOption;
+import com.dongyimai.bean.TbSpecificationOptionExample;
+import com.dongyimai.dao.TbSpecificationOptionMapper;
+import com.dongyimai.group.Specification;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
@@ -22,7 +27,8 @@ public class SpecificationServiceImpl implements SpecificationService {
 
 	@Autowired
 	private TbSpecificationMapper specificationMapper;
-	
+	@Autowired
+	private TbSpecificationOptionMapper specificationOptionMapper;
 	/**
 	 * 查询全部
 	 */
@@ -45,17 +51,33 @@ public class SpecificationServiceImpl implements SpecificationService {
 	 * 增加
 	 */
 	@Override
-	public void add(TbSpecification specification) {
-		specificationMapper.insert(specification);		
+	public void add(Specification specification) {
+		specificationMapper.insert(specification.getSpecification());
+
+		//循环插入规格选项
+		for(TbSpecificationOption specificationOption : specification.getSpecificationOptionList()){
+			//设置规格通过seleckkey标签获取到的id
+			specificationOption.setSpecId(specification.getSpecification().getId());
+			specificationOptionMapper.insert(specificationOption);
+		}
 	}
 
-	
 	/**
 	 * 修改
 	 */
 	@Override
-	public void update(TbSpecification specification){
-		specificationMapper.updateByPrimaryKey(specification);
+	public void update(Specification specification){
+		//保存修改的规格
+		specificationMapper.updateByPrimaryKey(specification.getSpecification());
+		//删除原有的规格选项
+		TbSpecificationOptionExample tbSpecificationOptionExample = new TbSpecificationOptionExample();
+		TbSpecificationOptionExample.Criteria tbSpecificationOptionExample_criteria = tbSpecificationOptionExample.createCriteria();
+		tbSpecificationOptionExample_criteria.andSpecIdEqualTo(specification.getSpecification().getId());
+		specificationOptionMapper.deleteByExample(tbSpecificationOptionExample);
+		//将新的规格选项存入
+		for (TbSpecificationOption tbSpecificationOption:specification.getSpecificationOptionList()){
+			specificationOptionMapper.insert(tbSpecificationOption);
+		}
 	}	
 	
 	/**
@@ -64,8 +86,17 @@ public class SpecificationServiceImpl implements SpecificationService {
 	 * @return
 	 */
 	@Override
-	public TbSpecification findOne(Long id){
-		return specificationMapper.selectByPrimaryKey(id);
+	public Specification findOne(Long id){
+		Specification specificationGroup = new Specification();
+		//查询规格并存储到复合实体类
+		specificationGroup.setSpecification(specificationMapper.selectByPrimaryKey(id));
+		//查询规格内容并存储到复合实体类
+		TbSpecificationOptionExample tbSpecificationOptionExample = new TbSpecificationOptionExample();
+		TbSpecificationOptionExample.Criteria tbSpecificationOptionExample_criteria = tbSpecificationOptionExample.createCriteria();
+		tbSpecificationOptionExample_criteria.andSpecIdEqualTo(id);
+		specificationGroup.setSpecificationOptionList(specificationOptionMapper.selectByExample(tbSpecificationOptionExample));
+		//返回复合实体类
+		return specificationGroup;
 	}
 
 	/**
