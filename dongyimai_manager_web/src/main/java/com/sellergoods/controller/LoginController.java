37471665315b4a45b87f8a4fae3f2a4d927ad539
Henry <henry_lin_@hotmail.com>
2021-05-03 12:35:32 +0800

    添加index页的用户名称显示，与注销

---
+++ dongyimai_manager_web/src/main/java/com/sellergoods/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.sellergoods.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    /**
+     * 返回登录的用户名
+     * @return
+     */
+    @RequestMapping("name")
+    public Map name(){
+        //从SecurityContextHolder中获取登陆的姓名
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Map map = new HashMap();
+        map.put("loginName",name);
+        return map;
+    }
+}
